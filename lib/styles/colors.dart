97e4094defb310c53201956c00c898fb9ba43b90
Yoshua Purba <yoshuapurba06@gmail.com>
2022-03-03 14:50:22 +0700

    first-commit

---
+++ lib/styles/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color mainGrey = Color(0xFF8E939D);
+  static const Color secondGrey = Color(0xFFB9B7BC);
+  static const Color mainBlue = Color(0xFF4471ED);
+  static const Color activitiesCardBg = Color(0xFFF8FAFB);
+  static const Color grey = Color(0xFFD8E1FF);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color green = Color(0xFF61BA56);
+  static const Color greenText = Color(0xFF1FBC92);
+  static const Color orange = Color(0xFFFA9E05);
+  static const Color red = Color(0xFFD86C73);
+}
---
+++ lib/styles/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static TextStyle regular12 = GoogleFonts.poppins(
+    fontSize: 12,
+  );
+
+  static TextStyle medium12 = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle medium14 = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle medium16 = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle medium18 = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle medium20 = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+}
